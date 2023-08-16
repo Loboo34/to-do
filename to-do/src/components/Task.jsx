cd6f72c81f6361fd38293a0af0a1a271290beaa1
loboo34 <rayovers40@gmail.com>
2023-08-16 23:12:25 +0300

    task

--- to-do/src/components/Task.jsx
+++ to-do/src/components/Task.jsx
@@ -10,17 +10,17 @@ const Task = () => {
       <label className=" relative flex space-x-3 items-center overlaycontainer ">
         <input type="radio" className=" rounded-full " />
         <div className=" items-center">
-          <p>my list:taks</p>
+          <p className=' text-slate-500'>my list:taks</p>
           <h1 className=" text-[22px]">name</h1>
         </div>
         <div>
           <FontAwesomeIcon
             icon={faTrashCan}
-            className=" absolute hidden right-0 pr-8 text-blue-700 overlay"
+            className=" absolute hidden right-0 pr-6 text-blue-700 transition ease-in-out hover:-translate-y-1 hover:scale-110 hover:text-red-500 overlay"
           />
           <FontAwesomeIcon
             icon={faThumbTack}
-            className=" absolute hidden right-10 pr-6 text-blue-700 rotate-45 pt-3 overlay"
+            className=" absolute hidden right-10 pr-6 text-blue-700 transition ease-in-out hover:-translate-y-1 hover:scale-110  rotate-45 pt-3 overlay"
           />
         </div>
       </label>
