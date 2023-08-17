056a7f341b40c4157d3c0287a3413485d9fb1da8
loboo34 <rayovers40@gmail.com>
2023-08-17 08:44:22 +0300

    log

--- to-do/frontend/src/pages/Login.js
+++ to-do/frontend/src/pages/Login.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
- 
+
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <div className="flex justify-center items-center bg-red-400 h-screen">
-      <div className=" relative w-[500px] h-[400px]  bg-[black] text-white flex flex-col justify-center items-center pt-10 pb-5 mt-[100px] rounded-md">
+    <div className="flex justify-center items-center bg-blue-700 h-screen">
+      <div className=" relative w-[500px] h-[400px]  bg-[black] text-white flex flex-col justify-center items-center  pb-5  rounded-md">
         <h1 className=" text-center text-[22px]">Sign In</h1>
         <form className="  w-[80%] ">
           <div className=" w-[100%]">
@@ -13,19 +16,25 @@ const Login = () => {
               <input
                 type="email"
                 placeholder="Enter your Email"
-                className=" w-full h-[50%]  mr-2"
+                value={email}
+                id="email"
+                className=" w-full h-[50%] mr-2 text-black"
                 required
+                onChange={(e) => setEmail(e.target.value)}
               />
             </div>
             <div className=" pb-2 justify-center flex flex-col h-[70px]">
               <label className=" text-[20px] pb-1">Password</label>
               <input
                 type="password"
-                className=" w-full h-[50%]  mr-2"
+                value={password}
+                id="email"
+                className=" w-full h-[50%] mr-2 text-black"
                 required
+                onChange={(e) => setPassword(e.target.value)}
               />
             </div>
-            <div className=" text-white bg-blue-600 text-center flex items-center justify-center h-[30px] mt-4">
+            <div className=" text-white bg-blue-600 text-center flex items-center justify-center h-[30px] mt-4 font-bold">
               <input type="submit" value="Sing In" />
             </div>
           </div>
@@ -41,6 +50,6 @@ const Login = () => {
       </div>
     </div>
   );
-}
+};
 
-export default Login
+export default Login;
--- to-do/frontend/src/pages/Singup.js
+++ to-do/frontend/src/pages/Singup.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 
 const Singup = () => {
+const [name, setName] = useState('')
+const [email, setEmail] = useState('')
+const [Password, setPassword] = useState('')
+
   return (
     <div className=" flex justify-center items-center bg-[#0000ffa0] h-screen ">
-      <div className=" relative w-[500px]  bg-[black] text-white flex flex-col justify-center items-center pt-10 pb-10 mt-[100px] rounded-md">
+      <div className=" relative w-[500px]  bg-[black] text-white flex flex-col justify-center items-center pt-10 pb-10  rounded-md">
         <h1 className=" text-center text-[22px] pb-4">Create Account</h1>
         <form className="  w-[80%] ">
           <div className=" w-[100%]">
@@ -12,29 +16,37 @@ const Singup = () => {
               <label className=" text-[20px] pb-1">Name</label>
               <input
                 type="name"
+                value={name}
                 placeholder="Enter your Name"
-                className=" w-full h-[50%] mr-2"
+                className=" w-full h-[50%] mr-2 text-black"
                 required
+                onChange={(e) => setName(e.target.value)}
               />
             </div>
             <div className=" pb-2 justify-center flex flex-col h-[70px]">
               <label className=" text-[20px] pb-1">Email</label>
               <input
                 type="email"
+                value={email}
+                id="email"
                 placeholder="Enter your Email"
-                className=" w-full h-[50%]  mr-2"
+                className=" w-full h-[50%]  mr-2 text-black"
                 required
+                onChange={(e) => setEmail(e.target.value)}
               />
             </div>
             <div className=" pb-2 justify-center flex flex-col h-[70px]">
               <label className=" text-[20px] pb-1">Password</label>
               <input
                 type="password"
-                className=" w-full h-[50%]  mr-2"
+                value={Password}
+                id="password"
+                className=" w-full h-[50%]  mr-2 text-black"
                 required
+                onChange={(e) => setPassword(e.target.value)}
               />
             </div>
-            <div className=" text-white bg-blue-600 text-center flex items-center justify-center h-[30px] mt-4">
+            <div className=" text-white bg-blue-600 text-center flex items-center justify-center h-[30px] mt-8 font-bold">
               <input type="submit" value="Sing up" />
             </div>
           </div>
