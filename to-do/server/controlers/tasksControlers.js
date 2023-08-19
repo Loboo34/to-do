a868c52a2c81f68f95577cde3acfea70ccf05d20
loboo34 <rayovers40@gmail.com>
2023-08-19 23:09:46 +0300

    controlers

---
+++ to-do/server/controlers/tasksControlers.js
@@ -0,0 +1,51 @@
+
+const Tasks = require("../models/Tasksmodel");
+const mongoose = require("mongoose");
+
+//get all tasks
+const getTasks = async(req, res) => {
+    const tasks = await Tasks.find({}).sort({cratedAt: -1})
+
+    res.status(200).json(tasks)
+}
+
+//get single task
+const getTask = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "No such Task" });
+  }
+
+  const task = await Tasks.findById(id);
+
+  if (!task) {
+    return res.status(404).json({ error: "No such Task" });
+  }
+
+  res.status(200).json(task);
+};
+
+//add task
+const addTask = async(req, res) => {
+    const { title, type, date, time } = req.body;
+
+    try {
+      const task = await Tasks.create({ title, type, date, time });
+      res.status(200).json(task);
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    } 
+}
+
+//delete task
+const deleteTask = async(req,res) => {
+    
+}
+
+module.exports = {
+    getTasks,
+    getTask,
+    addTask
+    
+}
--- to-do/server/routes/tasks.js
+++ to-do/server/routes/tasks.js
@@ -1,29 +1,16 @@
 const express = require("express");
-const Task = require('../models/Tasksmodel')
+const { addTask, getTasks, getTask} = require("../controlers/tasksControlers")
 
 const router = express.Router();
 
 //gget all tsks
-router.get("/", (req, res) => {
-  res.json({ mssg: "come get some" });
-});
+router.get("/", getTasks);
 
 //get single workout
-router.get("/:id", (req, res) => {
-    res.json({ mssg: "come get some" });
-});
+router.get("/:id", getTask);
 
 //post task
-router.post("/", async(req, res) => {
-  const {title, type, date, time } = req.body;
-
-try{
-const task = await Task.create({title, type, date, time})
-res.status(200).json(task)
-} catch(error){
-    res.status(400).json({message: error.message})
-} 
-});
+router.post("/", addTask);
 
 //delete task
 router.delete("/:id", (req, res) => {
