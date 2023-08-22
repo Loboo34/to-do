57c858b2301c66d44f5794f9992ee14194487b47
loboo34 <rayovers40@gmail.com>
2023-08-22 23:46:13 +0300

    fetch

--- to-do/frontend/src/pages/Home.jsx
+++ to-do/frontend/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
 import Task from '../components/Task'
 import Sidebar from '../components/Sidebar'
@@ -6,6 +6,19 @@ import AddTask from '../components/AddTask'
 
 
 const Home = () => {
+const [tasks, setTasks] = useState(null);
+
+    useEffect(() => {
+      const fetchTasks = async () => {
+        const response = await fetch("/api/tasks/");
+        const json = await response.json();
+
+        if (response.ok) {
+          setTasks(json);
+        }
+      };
+      fetchTasks();
+    }, []);
   return (
     <div className=" bg-slate-200">
       <Navbar />
@@ -21,16 +34,13 @@ const Home = () => {
               <span className=" text-center text-[22px] font-bold">13</span>
               <span className=" text-[15px] text-slate-400">August</span>
             </div>
-            <div className=" flex flex-col leading-5 pt-4" >
+            <div className=" flex flex-col leading-5 pt-4">
               <span>6:45p.m - 8:00p.m</span>
               <p>task due: read </p>
             </div>
           </div>
-          <Task />
-          <Task />
-          <Task />
-          <Task />
-          
+
+          {tasks && tasks.map((task) => <Task key={task._id} task={task}/>)}
         </div>
         <div className=" w-3/12 max-md:hidden">
           <AddTask />
